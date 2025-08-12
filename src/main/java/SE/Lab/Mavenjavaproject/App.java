07310765e65d7e4a1dfcde3fa4f2eebfd7e0ec0d
Vinesh <nvinesh58@gmail.com>
2025-08-12 23:09:12 +0530

    First commit push java project

---
+++ src/main/java/SE/Lab/Mavenjavaproject/App.java
@@ -0,0 +1,10 @@
+package SE.Lab.Mavenjavaproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This is First Maven Java Project By 23BD1A663B.");
+    }
+}
